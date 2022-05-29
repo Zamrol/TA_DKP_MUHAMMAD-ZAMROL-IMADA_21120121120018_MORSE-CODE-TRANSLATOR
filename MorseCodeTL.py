25b47c4f46c495f0748ef5739a008dbd45020980
Muhammad Zamrol Imada <91647148+Zamrol@users.noreply.github.com>
2022-05-29 18:27:51 +0700

    Update MorseCodeTL.py

--- MorseCodeTL.py
+++ MorseCodeTL.py
@@ -4,25 +4,7 @@ from tkinter import messagebox
 window = Tk()
 window.title("Morse Code TL")
 window.configure(bg = '#c0c0c0')
-window.geometry("1200x400")
-
-#Label
-labTitle = Label(window, text= "Morse Code Translator",
-                font=("comic sans ms", 30),
-                bg= 'white', borderwidth= 3, 
-                relief='groove', padx= 5).place(x = 380, y = 5) 
-labInput = Label(window, text= "Input",
-                font=("comic sans ms", 15),
-                padx= 5, bg='#c0c0c0').place(x = 180, y = 55) 
-labOutput = Label(window, text= "Output",
-                font=("comic sans ms", 15),
-                padx= 5, bg='#c0c0c0').place(x = 880, y = 55) 
-
-#TextField
-tfInput  = Entry(window ,width=40, borderwidth=5, font = ("comic sans ms", 14))
-tfOutput = Entry(window, width=40, borderwidth=5, font = ("times new roman", 15))
-tfInput.place(x=30, y=100)
-tfOutput.place(x=720, y=100)
+window.geometry("1200x250")
 
 AtoM = {'A': '.-'   , 'B': '-...'   , 'C': '-.-.'   , 'D': '-..', 
         'E': '.'    , 'F': '..-.'   , 'G': '--.'    , 'H': '....',
@@ -40,17 +22,17 @@ for key, value in AtoM.items():
 
 def atom(msg):
     morse = []
-    for char in msg:
-        if char in AtoM:
-            morse.append(AtoM[char])
+    for i in msg:
+        if i in AtoM:
+            morse.append(AtoM[i])
     return " ".join(morse)
 
 def mtoa(msg):
     msg = msg.split(" ")
     alph = []
-    for code in msg:
-        if code in MtoA:
-            alph.append(MtoA[code])
+    for i in msg:
+        if i in MtoA:
+            alph.append(MtoA[i])
     return " ".join(alph)
 
 def main1():
@@ -75,13 +57,29 @@ def main2():
 def cls():
     tfInput.delete(0, END)
     tfOutput.delete(0, END)
+    
 
-b1 = Button(window, text="Alphanumeric to Morse", command=main1)
-b2 = Button(window, text="Morse to Alphanumeric", command=main2)
-b3 = Button(window, text="Clear", command=cls, width= 30)
-b1.place(x=550, y=100)
-b2.place(x=550, y=140)
-b3.place(x=510, y=200)
+#Label
+labTitle = Label(window, text= "Morse Code Translator",
+                font=("comic sans ms", 30),
+                bg= 'white', borderwidth= 3, 
+                relief='groove', padx= 5).place(x = 380, y = 5) 
+labInput = Label(window, text= "Input",
+                font=("comic sans ms", 15),
+                padx= 5, bg='#c0c0c0').place(x = 180, y = 55) 
+labOutput = Label(window, text= "Output",
+                font=("comic sans ms", 15),
+                padx= 5, bg='#c0c0c0').place(x = 880, y = 55) 
+
+#TextField
+tfInput  = Entry(window ,width=40, borderwidth=5, font = ("comic sans ms", 14))
+tfOutput = Entry(window, width=40, borderwidth=5, font = ("times new roman", 15))
+tfInput.place(x=30, y=100)
+tfOutput.place(x=720, y=100)
+
+#button
+b1 = Button(window, text="Alphabet to Morse", command=main1).place(x=565, y=100)
+b2 = Button(window, text="Morse to Alphabet", command=main2).place(x=565, y=140)
+b3 = Button(window, text="Clear", command=cls, width= 30).place(x=510,y=200)
 
 window.mainloop()
-
